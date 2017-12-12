d51a76098d5463c14522ff68a096c7b08d82302d
Pavan Kumar C <pavan.challa23@gmail.com>
2017-12-13 01:05:05 +0530

    created new project and added a tableView

---
+++ SectionalExpandableCollapsable/ViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ViewCell.swift
+//  SectionalExpandableCollapsable
+//
+//  Created by Pavan Kumar C on 13/12/17.
+//  Copyright © 2017 pavan. All rights reserved.
+//
+
+import UIKit
+
+class ViewCell: UITableViewCell {
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    // Initialization code
+  }
+  
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+    
+    // Configure the view for the selected state
+  }
+  
+}
---
+++ SectionalExpandableCollapsable/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SectionalExpandableCollapsable
+//
+//  Created by Pavan Kumar C on 13/12/17.
+//  Copyright © 2017 pavan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
