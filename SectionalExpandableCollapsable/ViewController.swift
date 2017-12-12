325cba00190dd139f5e9a78e31584793463f35aa
Pavan Kumar C <pavan.challa23@gmail.com>
2017-12-13 01:35:11 +0530

    Countries database added

--- SectionalExpandableCollapsable/ViewController.swift
+++ SectionalExpandableCollapsable/ViewController.swift
@@ -10,15 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  var myDict: NSDictionary?
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    if let path = Bundle.main.path(forResource: "Countries", ofType: "plist") {
+      myDict = NSDictionary(contentsOfFile: path)
+    }
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
   }
-
 
 }
